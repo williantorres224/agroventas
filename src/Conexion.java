6e3d2ccc9a65ae17288ff819d3e90e7b56860d31
Willian Torres <williantorres224@gmail.com>
2026-04-21 10:26:35 -0500

    Remove database credentials (security fix)

--- src/Conexion.java
+++ src/Conexion.java
@@ -5,11 +5,12 @@ public class Conexion {
 
     private static final String URL =
         "jdbc:mysql://localhost:3306/agroventas?useSSL=false&allowPublicKeyRetrieval=true";
-    private static final String USER = "root";
-    private static final String PASS = "Mysql1234*TORRES";
 
-public static Connection getConexion() throws Exception {
-    Class.forName("com.mysql.cj.jdbc.Driver"); 
-    return DriverManager.getConnection(URL, USER, PASS);
-}
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASS = System.getenv("DB_PASS");
+
+    public static Connection getConexion() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
